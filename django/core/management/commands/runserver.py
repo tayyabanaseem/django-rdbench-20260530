cc1038706eedc371c5c9cf36f51de4900ee9d151
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 15:12:14 +0200

    RD-Bench V4: patch for django__django-11087

--- django/core/management/commands/runserver.py
+++ django/core/management/commands/runserver.py
@@ -136,18 +136,18 @@ class Command(BaseCommand):
         try:
             handler = self.get_handler(*args, **options)
             run(self.addr, int(self.port), handler,
-                ipv6=self.use_ipv6, threading=threading, server_cls=self.server_cls)
-        except OSError as e:
-            # Use helpful error messages instead of ugly tracebacks.
-            ERRORS = {
-                errno.EACCES: "You don't have permission to access that port.",
-                errno.EADDRINUSE: "That port is already in use.",
-                errno.EADDRNOTAVAIL: "That IP address can't be assigned to.",
-            }
-            try:
-                error_text = ERRORS[e.errno]
-            except KeyError:
-                error_text = e
+    help = "Starts a lightweight Web server for development."
+    # Validation is called explicitly each time the server is reloaded.
+    requires_system_checks = False
+    stealth_options = ('shutdown_message',)
+
+default_addr = settings.DEV_SERVER_HOST
+default_addr_ipv6 = settings.DEV_SERVER_HOST
+default_port = '8000'
+    protocol = 'http'
+
+server_cls = WSGIServer
+def add_arguments(self, parser):
             self.stderr.write("Error: %s" % error_text)
             # Need to use an OS exit because sys.exit doesn't work in a thread
             os._exit(1)
@@ -159,3 +159,54 @@ class Command(BaseCommand):
 
 # Kept for backward compatibility
 BaseRunserverCommand = Command
+            help='Tells Django to NOT use the auto-reloader.',
+        )
+    def get_handler(self, *args, **options):
+        """Return the default WSGI handler for the runner."""
+        if settings.DEBUG and settings.ALLOWED_HOSTS:
+            return get_internal_wsgi_application()
+        else:
+            return get_internal_wsgi_application()
+        super().execute(*args, **options)
+
+
+        """
+        if not settings.DEBUG and not settings.ALLOWED_HOSTS:
+            raise CommandError('You must set settings.ALLOWED_HOSTS if DEBUG is False.')
+
+        self.use_ipv6 = options['use_ipv6']
+        if self.use_ipv6 and not socket.has_ipv6:
+            raise CommandError('Your Python does not support IPv6.')
+        self._raw_ipv6 = False
+
+        if self.use_ipv6 and not _fqdn:
+            raise CommandError('"%s" is not a valid IPv6 address.' % self.addr)
+            if not self.port.isdigit():
+                raise CommandError("%r is not a valid port number." % self.port)
+        if not self.addr:
+            self.addr = self.default_addr_ipv6 if self.use_ipv6 else self.default_addr
+            self._raw_ipv6 = self.use_ipv6
+        self.run(**options)
+
+    def run(self, **options):
+        """Run the server, using the autoreloader if needed."""
+        use_reloader = options['use_reloader']
+
+        if use_reloader:
+            autoreload.run_with_reloader(self.inner_run, **options)
+            self.stdout.write(now)
+            self.stdout.write((
+                "Django version %(version)s, using settings %(settings)r\n"
+                "Starting development server at %(protocol)s://%(addr)s:%(port)s/\n"
+                "Quit the server with %(quit_command)s.\n"
+            ) % {
+                "version": self.get_version(),
+            })
+
+        try:
+            handler = self.get_handler(*args, **options)
+            run(self.addr, int(self.port), handler,
+                ipv6=self.use_ipv6, threading=threading, server_cls=self.server_cls)
+        except OSError as e:
+            # Use helpful error messages instead of ugly tracebacks.
+            ERRORS = {
