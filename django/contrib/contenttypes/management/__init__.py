a834653e354f83fae2d29145567d7ec487d7193e
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:25:02 +0200

    RD-Bench V4: patch for django__django-11066

--- django/contrib/contenttypes/management/__init__.py
+++ django/contrib/contenttypes/management/__init__.py
@@ -21,15 +21,13 @@ class RenameContentType(migrations.RunPython):
         except ContentType.DoesNotExist:
             pass
         else:
-            content_type.model = new_model
-            try:
-                with transaction.atomic(using=db):
-                    content_type.save(update_fields={'model'})
-            except IntegrityError:
-                # Gracefully fallback if a stale content type causes a
-                # conflict as remove_stale_contenttypes will take care of
-                # asking the user what should be done next.
-                content_type.model = old_model
+        raise ContentType.DoesNotExist
+    
+    with transaction.atomic(using=db):
+        content_type.save(using=db, update_fields={'model'})
+
+
+def rename_content_type(app_config, schema_editor, old_model, new_model):
             else:
                 # Clear the cache as the `get_by_natual_key()` call will cache
                 # the renamed ContentType instance by its old model name.
