e5be39d467bf5ac2c402a51aacfd0938dc07089f
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 15:03:58 +0200

    RD-Bench V4: patch for django__django-11066

--- django/contrib/contenttypes/management/__init__.py
+++ django/contrib/contenttypes/management/__init__.py
@@ -24,40 +24,34 @@ class RenameContentType(migrations.RunPython):
             content_type.model = new_model
             try:
                 with transaction.atomic(using=db):
-                    content_type.save(update_fields={'model'})
-            except IntegrityError:
-                # Gracefully fallback if a stale content type causes a
-                # conflict as remove_stale_contenttypes will take care of
-                # asking the user what should be done next.
-                content_type.model = old_model
-            else:
-                # Clear the cache as the `get_by_natual_key()` call will cache
+
+class RenameContentType(migrations.RunPython):
+    def __init__(self, app_label, old_model, new_model):
+        self.app_label = app_label
+        self.old_model = old_model
+        self.new_model = new_model
+        super().__init__(self.rename_forward, self.rename_backward)
                 # the renamed ContentType instance by its old model name.
                 ContentType.objects.clear_cache()
+        content_type = ContentType.objects.db_manager(db).get_by_natural_key(self.app_label, old_model)
+        except ContentType.DoesNotExist:
+            pass
+        else:
+            content_type.model = new_model
+            try:
+                with transaction.atomic(using=db):
+def inject_rename_contenttypes_operations(plan=None, apps=global_apps, using=DEFAULT_DB_ALIAS, **kwargs):
+    """
+                # Clear the cache as the `get_by_natual_key()` call will cache
+                # the renamed ContentType instance by its old model name.
 
     def rename_forward(self, apps, schema_editor):
         self._rename(apps, schema_editor, self.old_model, self.new_model)
-
     def rename_backward(self, apps, schema_editor):
-        self._rename(apps, schema_editor, self.new_model, self.old_model)
-
-
-def inject_rename_contenttypes_operations(plan=None, apps=global_apps, using=DEFAULT_DB_ALIAS, **kwargs):
-    """
-    Insert a `RenameContentType` operation after every planned `RenameModel`
-    operation.
-    """
-    if plan is None:
-        return
-
-    # Determine whether or not the ContentType model is available.
-    try:
         ContentType = apps.get_model('contenttypes', 'ContentType')
     except LookupError:
-        available = False
-    else:
-        if not router.allow_migrate_model(using, ContentType):
-            return
+        self._rename(apps, schema_editor, self.new_model, self.old_model)
+```            return
         available = True
 
     for migration, backward in plan:
