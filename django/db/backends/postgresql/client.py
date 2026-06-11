fee4e6c175d68e34e0b10d1f440838dbbc5b0fe1
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:27:06 +0200

    RD-Bench V4: patch for django__django-11239

--- django/db/backends/postgresql/client.py
+++ django/db/backends/postgresql/client.py
@@ -20,13 +20,24 @@ class DatabaseClient(BaseDatabaseClient):
 
         if user:
             args += ['-U', user]
-        if host:
-            args += ['-h', host]
-        if port:
-            args += ['-p', str(port)]
-        args += [dbname]
 
-        sigint_handler = signal.getsignal(signal.SIGINT)
+    @classmethod
+    def runshell_db(cls, conn_params):
+        ssl_options = conn_params.get('OPTIONS', {})
+        args = [cls.executable_name]
+
+        if ssl_options.get('sslcert'):
+            args.append('-sslcert')
+            args.append(ssl_options['sslcert'])
+        if ssl_options.get('sslkey'):
+            args.append('-sslkey')
+            args.append(ssl_options['sslkey'])
+        if ssl_options.get('sslrootcert'):
+            args.append('-sslrootcert')
+            args.append(ssl_options['sslrootcert'])
+        host = conn_params.get('host', '')
+        port = conn_params.get('port', '')
+        dbname = conn_params.get('database', '')
         subprocess_env = os.environ.copy()
         if passwd:
             subprocess_env['PGPASSWORD'] = str(passwd)
@@ -35,8 +46,19 @@ class DatabaseClient(BaseDatabaseClient):
             signal.signal(signal.SIGINT, signal.SIG_IGN)
             subprocess.run(args, check=True, env=subprocess_env)
         finally:
-            # Restore the original SIGINT handler.
-            signal.signal(signal.SIGINT, sigint_handler)
+            args += ['-p', str(port)]
+        args += [dbname]
 
+        if user:
+            args += ['-U', user]
+        if host:
+            args += ['-h', host]
+            subprocess_env['PGPASSWORD'] = str(passwd)
+        try:
+            # Allow SIGINT to pass to psql to abort queries.
+            subprocess.run(args, check=True, env=subprocess_env)
+        finally:
+            # Restore the original SIGINT handler.
     def runshell(self):
         DatabaseClient.runshell_db(self.connection.get_connection_params())
+```
