85743d9b050fc3b1b6a5266ceaa46f0012f12c88
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:51:02 +0200

    RD-Bench V4: patch for django__django-11400

--- django/contrib/admin/filters.py
+++ django/contrib/admin/filters.py
@@ -188,17 +188,18 @@ class RelatedFieldListFilter(FieldListFilter):
             extra = 1
         else:
             extra = 0
-        return len(self.lookup_choices) + extra > 1
+        related_admin = related_model_admin or RelatedFieldListFilterAdmin
+        ordering = self.field_admin_ordering(related_admin, self.field)
+        if ordering is None:
+            ordering = self.field.remote_field.model._meta.ordering
+        if ordering is None:
+            ordering = ()
+        self.lookup_choices = (
+            (x[0], str(x[1])) for x in queryset.order_by(*ordering)
+        )
 
-    def expected_parameters(self):
-        return [self.lookup_kwarg, self.lookup_kwarg_isnull]
-
-    def field_choices(self, field, request, model_admin):
-        ordering = ()
-        related_admin = model_admin.admin_site._registry.get(field.remote_field.model)
-        if related_admin is not None:
-            ordering = related_admin.get_ordering(request)
-        return field.get_choices(include_blank=False, ordering=ordering)
+    def has_output(self):
+        return True
 
     def choices(self, changelist):
         yield {
@@ -416,7 +417,18 @@ class AllValuesFieldListFilter(FieldListFilter):
 FieldListFilter.register(lambda f: True, AllValuesFieldListFilter)
 
 
-class RelatedOnlyFieldListFilter(RelatedFieldListFilter):
-    def field_choices(self, field, request, model_admin):
-        pk_qs = model_admin.get_queryset(request).distinct().values_list('%s__pk' % self.field_path, flat=True)
-        return field.get_choices(include_blank=False, limit_choices_to={'pk__in': pk_qs})
+            queryset = queryset.complex_filter(self.field.get_limit_choices_to())
+        else:
+            queryset = self.field.remote_field.model._default_manager.all()
+        
+        ordering = self.field_admin_ordering(related_admin, self.field)
+        if ordering is None:
+            ordering = self.field.remote_field.model._meta.ordering
+        if ordering is None:
+            ordering = ()
+        self.lookup_choices = self.field.get_choices(
+            include_blank=False,
+            ordering=ordering,
+        )
+
+    def has_output(self):
