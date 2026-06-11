5ed72a1eca4a7ef72bed2ad1896c88a40a91cf14
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:16:29 +0200

    RD-Bench V4: patch for django__django-10880

--- django/db/models/aggregates.py
+++ django/db/models/aggregates.py
@@ -64,13 +64,11 @@ class Aggregate(Func):
             return '%s__%s' % (expressions[0].name, self.name.lower())
         raise TypeError("Complex expressions require an alias")
 
-    def get_group_by_cols(self):
-        return []
-
     def as_sql(self, compiler, connection, **extra_context):
-        extra_context['distinct'] = 'DISTINCT' if self.distinct else ''
+        extra_context['distinct'] = ' DISTINCT ' if self.distinct else ''
         if self.filter:
             if connection.features.supports_aggregate_filter_clause:
+                filter_sql, filter_params = self.filter.as_sql(compiler, connection)
                 filter_sql, filter_params = self.filter.as_sql(compiler, connection)
                 template = self.filter_template % extra_context.get('template', self.template)
                 sql, params = super().as_sql(
