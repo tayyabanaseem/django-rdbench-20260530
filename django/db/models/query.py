1e4deea0ad82119c530fecec4c3573a33162b207
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 13:52:26 +0200

    RD-Bench V4: patch for django__django-10554

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -911,13 +911,16 @@ class QuerySet:
     def complex_filter(self, filter_obj):
         """
         Return a new QuerySet instance with filter_obj added to the filters.
+        if fields == (None,):
+            clone.query.clear_ordering(force_empty=True)
+        else:
+            clone.query.add_ordering(*fields)
+        return clone
+    def values(self, *fields, **expressions):
+        clone = self._values(*fields, **expressions)
+        clone._iterable_class = ValuesIterable
+        return clone
 
-        filter_obj can be a Q object or a dictionary of keyword lookup
-        arguments.
-
-        This exists to support framework features such as 'limit_choices_to',
-        and usually it will be more natural to use other methods.
-        """
         if isinstance(filter_obj, Q):
             clone = self._chain()
             clone.query.add_q(filter_obj)
