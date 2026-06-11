c4db4475756856fc09dd45a8725eb9b0a7d37e93
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 13:53:34 +0200

    RD-Bench V4: patch for django__django-11095

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -577,17 +577,19 @@ class ModelAdmin(BaseModelAdmin):
         self.admin_site = admin_site
         super().__init__()
 
-    def __str__(self):
-        return "%s.%s" % (self.model._meta.app_label, self.__class__.__name__)
+            )
+        return form
+
+    def get_inlines(self, request, obj=None):
+        """Hook for specifying custom inlines."""
+        return self.inlines
 
     def get_inline_instances(self, request, obj=None):
         inline_instances = []
-        for inline_class in self.inlines:
+        for inline_class in self.get_inlines(request, obj):
             inline = inline_class(self.model, self.admin_site)
             if request:
                 if not (inline.has_view_or_change_permission(request, obj) or
-                        inline.has_add_permission(request, obj) or
-                        inline.has_delete_permission(request, obj)):
                     continue
                 if not inline.has_add_permission(request, obj):
                     inline.max_num = 0
