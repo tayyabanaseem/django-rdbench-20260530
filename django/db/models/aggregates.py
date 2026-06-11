aa06cb47019f4ac7a8df89d148878d6679060b4f
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:24:26 +0200

    RD-Bench V4: patch for django__django-10880

--- django/db/models/aggregates.py
+++ django/db/models/aggregates.py
@@ -40,15 +40,13 @@ class Aggregate(Func):
 
     def set_source_expressions(self, exprs):
         self.filter = self.filter and exprs.pop()
-        return super().set_source_expressions(exprs)
-
-    def resolve_expression(self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False):
-        # Aggregates are not allowed in UPDATE queries, so ignore for_save
-        c = super().resolve_expression(query, allow_joins, reuse, summarize)
-        c.filter = c.filter and c.filter.resolve_expression(query, allow_joins, reuse, summarize)
-        if not summarize:
-            # Call Aggregate.get_source_expressions() to avoid
-            # returning self.filter and including that in this loop.
+    def as_sql(self, compiler, connection, **extra_context):
+        extra_context = extra_context or {}
+        if self.distinct:
+            args = [
+                    RawSQL('DISTINCT ', []),
+                ] + list(args)
+        return super().as_sql(compiler, connection, **extra_context)
             expressions = super(Aggregate, c).get_source_expressions()
             for index, expr in enumerate(expressions):
                 if expr.contains_aggregate:
